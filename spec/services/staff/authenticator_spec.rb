3cd0320a265278fd771a4899175e1062670a5f7f
Shoichi Imamura <syoichiimamura@gmail.com>
2016-08-28 10:08:50 +0900

    factoryによる認証テストの実行

---
+++ spec/services/staff/authenticator_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Staff::Authenticator do
+  describe "#authenticate" do
+    example '正しいパスワードならtrueを返す' do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate('pw')).to be_truthy
+    end
+
+    example '誤ったパスワードならfalseを返す' do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate('wp')).to be_falsey
+    end
+
+    example '停止フラグが立っていたならfalseを返す' do
+      m = build(:staff_member, suspended: true)
+      expect(Staff::Authenticator.new(m).authenticate('pw')).to be_falsey
+    end
+
+    example '終了後ならfalseを返す' do
+      m = build(:staff_member, end_date: Date.today)
+      expect(Staff::Authenticator.new(m).authenticate('pw')).to be_falsey
+    end
+  end
+
+end
