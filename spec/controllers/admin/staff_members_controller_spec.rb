d03672487f208e7016b03a47eb48ba17e895b93e
Shoichi Imamura <syoichiimamura@gmail.com>
2016-08-30 13:46:01 +0900

    StaffMembersControllerのspecファイルを実装

--- spec/controllers/admin/staff_members_controller_spec.rb
+++ spec/controllers/admin/staff_members_controller_spec.rb
@@ -13,4 +13,23 @@ RSpec.describe Admin::StaffMembersController do
       expect { post :create }.to raise_error(ActionController::ParameterMissing)
     end
   end
+
+  describe '#update' do
+    let(:staff_member) { create(:staff_member) }
+
+    example 'suspendedフラグをセットする' do
+      params_hash[:suspended] = true
+      patch :update, id: staff_member.id, staff_member: params_hash
+      staff_member.reload
+      expect(staff_member).to be_suspended
+    end
+
+    example 'haswd_passwordの値は書き換え不可' do
+      params_hash.delete(:password)
+      params_hash[:hashed_password] = 'x'
+      expect(
+        patch(:update, id: staff_member.id, staff_member: params_hash)
+      ).not_to change { staff_member.hashed_password.to_s }
+    end
+  end
 end
